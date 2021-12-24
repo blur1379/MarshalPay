7a25c1e751352d0248923853a30f934a80da72bb
blur1379 <blor1379@gmail.com>
2021-12-24 15:43:32 +0330

    Initial Commit

---
+++ Marshal/MarshalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarshalApp.swift
+//  Marshal
+//
+//  Created by Blur on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MarshalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
