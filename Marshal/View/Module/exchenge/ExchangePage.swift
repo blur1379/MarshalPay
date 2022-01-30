f06c0f6991e78b88965544b8e5eb2cb37a614013
blur1379 <blor1379@gmail.com>
2022-01-30 16:37:12 +0330

    Blur

---
+++ Marshal/View/Module/exchenge/ExchangePage.swift
@@ -0,0 +1,22 @@
+//
+//  ExchangePage.swift
+//  Marshal
+//
+//  Created by Blur on 11/9/1400 AP.
+//
+
+import SwiftUI
+
+struct ExchangePage: View {
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+struct ExchangePage_Previews: PreviewProvider {
+    static var previews: some View {
+        ExchangePage()
+    }
+}
